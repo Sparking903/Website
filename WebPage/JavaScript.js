6c32d3cca999298259d24a88ef4a4ad0793a251f
Sparking903 <168690216+Sparking903@users.noreply.github.com>
2024-06-22 12:09:20 -0500

    Delete WebPage directory

--- WebPage/JavaScript.js
+++
@@ -1,7 +0,0 @@
-ButtonElement = document.getElementById("Test")
-
-function W() {
-    ButtonElement.innerText = "Wow"
-    var objShell = new ActiveXObject("WScript.Shell");
-    objShell.Run("path_to_your_vbscript.vbs", 0, false);
-}
